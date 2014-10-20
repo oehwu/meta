8dd415483b04b8411bcf603f8b26925d6b5a8c15
Thomas Rieschl <thomas.rieschl@oeh-wu.at>
2014-10-20 09:29:25 +0200

    add example

---
+++ example.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'src/Header.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>OEH WU Header</title>
+</head>
+
+<body>
+
+<?= OEHWU\Header\Header::getHeader() ?>
+
+</body>
+</html>
