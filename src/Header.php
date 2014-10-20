c826c344e2eeaa3af9a4703ec9d032b396193336
Thomas Rieschl <thomas.rieschl@oeh-wu.at>
2014-10-20 09:07:19 +0200

    Initial Commit

---
+++ src/Header.php
@@ -0,0 +1,59 @@
+<?php
+namespace OEHWU\Header;
+
+/**
+ * Class Header
+ *
+ * Returns the OEH WU header
+ *
+ * @package OEHWU\Header
+ */
+class Header
+{
+
+    /**
+     * URI to fetch
+     */
+    const headerUrl = 'https://oeh-wu.at/snippets/header.html';
+
+    /**
+     * returns the header HTML as string or null if it fails
+     *
+     * @return string|null
+     */
+    public static function getHeader()
+    {
+        if (!self::hasCurl()) {
+            return null;
+        }
+
+        $c = curl_init();
+
+        curl_setopt($c, CURLOPT_HTTPGET, 1);
+        curl_setopt($c, CURLOPT_URL, self::headerUrl);
+        curl_setopt($c, CURLOPT_AUTOREFERER, 1);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+
+        $response = curl_exec($c);
+        $header = curl_getinfo($c);
+
+        curl_close($c);
+
+        if ($response === false) {
+            return null;
+        }
+
+        if ($header['http_code'] != 200) {
+            return null;
+        }
+
+        return $response;
+    }
+
+    private static function hasCurl()
+    {
+        return (function_exists('curl_init')) ? true : false;
+    }
+}
