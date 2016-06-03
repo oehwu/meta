62e1846ff731f914425f733848dd1200600384e0
Thomas Rieschl <thomas.rieschl@oeh-wu.at>
2016-06-03 11:20:04 +0200

    add simple cache

--- src/Header.php
+++ src/Header.php
@@ -12,19 +12,35 @@ class Header
 {
 
     /**
-     * URI to fetch
+     * @var string URI to fetch
      */
     const HEADER_URL = 'https://oeh-wu.at/snippets/header.html';
 
     /**
+     * @var string filename of temp file
+     */
+    const TMP_FILENAME = 'oehwu_web_header';
+
+    /**
+     * @var int cache time in seconds
+     */
+    const CACHE_TIME = 24 * 60 * 60;
+
+    /**
      * returns the header HTML as string, or null if it fails
      *
-     * @return string|null
+     * @param bool $forceNewLoad don't use a cached version
+     * @return null|string
      */
-    public static function getHeader()
+    public static function getHeader($forceNewLoad = false)
     {
         if (!self::hasCurl()) {
             return null;
+        }
+
+        $cache = self::fetchCache();
+        if (false === $forceNewLoad && null !== $cache) {
+            return $cache;
         }
 
         $c = curl_init();
@@ -49,11 +65,59 @@ class Header
             return null;
         }
 
+        self::writeCache($response);
+
         return $response;
     }
 
+    /**
+     * @return bool
+     */
     private static function hasCurl()
     {
         return (function_exists('curl_init')) ? true : false;
+    }
+
+    /**
+     * @return null|string
+     */
+    private static function fetchCache()
+    {
+        $fileName = self::getFileName();
+
+        if (!file_exists($fileName)) {
+            return null;
+        }
+
+        if (filemtime($fileName) + self::CACHE_TIME < time()) {
+            unlink($fileName);
+            return null;
+        }
+
+        return file_get_contents($fileName);
+    }
+
+    /**
+     * @param string $headerStr
+     */
+    private static function writeCache($headerStr)
+    {
+        $fileName = self::getFileName();
+
+        if (!is_writable($fileName)) {
+            return;
+        }
+
+        $res = fopen($fileName, 'w');
+        fwrite($res, $headerStr);
+        fclose(($res));
+    }
+
+    /**
+     * @return string
+     */
+    private static function getFileName()
+    {
+        return sys_get_temp_dir() . '/' . self::TMP_FILENAME;
     }
 }
